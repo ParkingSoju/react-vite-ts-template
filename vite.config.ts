94254348b6e44d5efd97238ecf7f6d00a9d6b50b
John Gamboa <johngamboa.email@gmail.com>
2025-06-25 07:56:49 +0800

    Added drop console and debugger on build

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,18 @@ import {defineConfig} from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig(({mode}) => ({
+	// base: added only for github pages,
 	base: '/react-vite-ts-template/',
-	plugins: [react()],
 	build: {
-		// outDir: 'build',
 		minify: 'esbuild',
 		target: 'es2022',
 		sourcemap: mode !== 'production',
 	},
+	esbuild: {
+		drop: ['console', 'debugger'],
+	},
+	plugins: [react()],
 	server: {
-		// port: 5173
 		strictPort: true,
 	},
 }));
